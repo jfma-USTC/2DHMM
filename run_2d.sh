cab1dc73fcd5a72e25ff78481f7077cfded4257a
JFMA <849090612@qq.com>
2020-11-17 14:07:35 +0800

    renew language files and some details in shell script

--- run_2d.sh
+++ run_2d.sh
@@ -13,13 +13,13 @@ zero=0
 train_tol_gauss=23400
 train_nj=20
 decode_nj=8
-train_data=./data_true_2d/train_8r_8d
-test_data=./data_true_2d/test_8r_8d
+train_data=./data_2d/train_8r_8d
+test_data=./data_2d/test_8r_8d
 #train_cmd=
 #decode_cmd= 
-topo_2d=./lang_true_2d/lang/topo_2d  # topo file for 2D situation
-lang=./lang_true_2d/lang
-exp_dir=./exp_true_2d/true_2d_r8_d8_11rows_after_force_state_of_lastrowcol
+topo_2d=./lang/lang_2d/topo_2d  # topo file for 2D situation
+lang=./lang/lang_2d
+exp_dir=./exp_2d/true_2d_r8_d8_11rows_after_force_state_of_lastrowcol
 decode_dir=$exp_dir/decode
 
 echo ============================================================================
@@ -28,7 +28,7 @@ echo ===========================================================================
 
 if [ $pre_lang -ne $zero ]; then
 	utils/prepare_lang.sh --position-dependent-phones false --num-sil-states 5 --num-nonsil-states 5 \
-		lang_true_2d/dict "<unk>" lang_true_2d/temp $lang
+		lang/lang_2d/dict "<unk>" lang/lang_2d/temp $lang
 	if [ -f $topo_2d ]; then 
 		cp $topo_2d $lang/topo
 	else
